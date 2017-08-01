2d9d1976191e1385130e1d815f9dde658e1d3a99
ritikamotwani <iit2015096@iiita.ac.in>
2017-08-02 03:53:52 +0530

    Deletion

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,10 @@
 $("li").click(function(){
 	$(this).toggleClass("completed");
 });
+$("span").click(function(){
+	
+	$(this).parent().fadeOut(500,function(){
+		remove();
+	});
+	event.stopPropagation();
+});
